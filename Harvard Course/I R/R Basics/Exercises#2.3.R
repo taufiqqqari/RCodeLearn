6df9fac1badd5fa465fe9f808d081dcdf292ae06
false-is-zero <taufiqq.arirahman@gmail.com>
2025-06-22 13:42:42 +0800

    Create Exercises#2.3.R
    The First Excercise

---
+++ Harvard Course/I R/R Basics/Exercises#2.3.R
@@ -0,0 +1,27 @@
+#Excercise on R Basic (I): 2.3 Excercise
+
+#Number 1
+n <- 100
+x <- seq(1, n)
+sum(x)
+
+#Number 2
+n <- 1000
+x <- seq(1, n)
+sum(x)
+
+#Number 3
+###=> Based on the function, Answear is: seq creates a list of numbers and sum adds them up.
+
+#Number 4
+log(base = 10, sqrt(100))
+
+#Number 5
+log(10^2)
+log10(2^10)
+log(exp(2))
+exp(log(2, base = 2))
+
+#If it as about Output that comeout is numeric, its gonnabe the C, log(exp(2))
+
+
